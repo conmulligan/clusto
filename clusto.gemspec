46c10b1e7dce50d9bd50415ae139c529051e5896
Conor Mulligan <conmulligan@gmail.com>
2012-10-14 02:15:44 +0100

    Variable grid size

--- clusto.gemspec
+++ clusto.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
-  s.name    = 'clusto'
-  s.version = '0.1.0'
-  s.author  = 'Conor Mulligan'
-  s.email   = 'conmulligan@gmail.com'
-  s.summary = 'Simple Ruby point clustering'
-
+  s.name     = 'clusto'
+  s.version  = '0.1.0'
+  s.author   = 'Conor Mulligan'
+  s.email    = 'conmulligan@gmail.com'
+  s.homepage = 'http://github.com/conmulligan/clusto'
+  s.summary  = 'Simple Ruby point clustering'
+  
   s.files = Dir["#{File.dirname(__FILE__)}/**/*"]
 end
--- lib/clusto.rb
+++ lib/clusto.rb
@@ -9,7 +9,7 @@ module Clusto
   RADIANS_PER_DEGREES = Math::PI / 180
   GREAT_CIRCLE_RADIUS = 6371
   
-  GRID_SIZE = 8
+  DEFAULT_GRID_SIZE = 8
 
   def minimum_bounding_rectangle(points)
     xs = points.collect { |p| p.x }.minmax
@@ -21,7 +21,8 @@ module Clusto
   def cluster(points, options={})
     bounds = options[:bounds] || Clusto.minimum_bounding_rectangle(points)
     distance_method = options[:distance_method] || :haversine
-
+    grid_size = options[:grid_size] || DEFAULT_GRID_SIZE
+    
     # Create clusters
     
     clusters = []
@@ -30,13 +31,13 @@ module Clusto
     y = bounds[3]
     w = bounds[2] - bounds[0]
     h = bounds[3] - bounds[1]
-    grid_w = w / GRID_SIZE
-    grid_h = h / GRID_SIZE
+    grid_w = w / grid_size
+    grid_h = h / grid_size
     
     column = 0
     row = 0
     
-    (GRID_SIZE ** 2).times do |i|
+    (grid_size ** 2).times do |i|
       offset_x = x + (column * grid_w)
       offset_y = y - (row * grid_h)
       
@@ -44,7 +45,7 @@ module Clusto
       clusters.push(Clusto::Cluster.new(center))
       
       column = column+1
-      if column == GRID_SIZE
+      if column == grid_size
         column = 0
         row = row+1
       end
@@ -74,8 +75,6 @@ module Clusto
 
     # Reposition clusters
     
-    clusters.each do |cluster|
-      cluster.reposition
-    end
+    clusters.each { |c| c.reposition }
   end
 end
